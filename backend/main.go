bdfca1ed00a25e98c31602e1f4bb02b03390dbc1
Thun (MacBook Pro) <bsthun@gmail.com>
2022-12-28 11:43:48 +0700

    Update province loader

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"backend/modules/config"
 	"backend/modules/fiber"
+	"backend/modules/hub"
 	"backend/modules/ldap"
 	"backend/modules/mysql"
 )
 
 func main() {
 	config.Init()
+	hub.Init()
 	mysql.Init()
 	ldap.Init()
 	fiber.Init()
